2d06bcdf7b9253c92b786a84de6a04edfcd27afc
nikitasuvorov2 nikitasuvorov2 <lialia2000@inbox.ru>
2022-02-07 04:05:20 +0500

    turned into function

--- check.py
+++ check.py
@@ -45,67 +45,73 @@ def get_number():
         value=input('Введите номер паспорта')
     return value
 
-model_name='final.h5'
-save_dir=os.getcwd() 
-model_path = os.path.join(save_dir, model_name)
-model = keras.models.load_model(model_path)
+def check(series,number):
+ TRIGGER_LINE="Cреди недействительных не значится"
+ ATTEMPTS=3
+ model_name='final.h5'
+ save_dir=os.getcwd() 
+ model_path = os.path.join(save_dir, model_name)
+ model = keras.models.load_model(model_path)
+ 
 
+ finish=False
 
-finish=False
+ #soup = BeautifulSoup(r.content, features="html.parser")
+ #print(soup)
 
-#soup = BeautifulSoup(r.content, features="html.parser")
-#print(soup)
+ serie=series#get_series()
+ number=number#get_number()
+ counter=0
+ while finish==False:
+     counter=counter+1
+     s=requests.session()
+     req = s.get('http://services.fms.gov.ru/services/captcha.jpg', stream=True)
 
-serie=get_series()
-number=get_number()
-
-while finish==False:
-    s=requests.session()
-    req = s.get('http://services.fms.gov.ru/services/captcha.jpg', stream=True)
-
-    req.raise_for_status()
-    with open('capthcas\captcha.jpg', 'wb') as fd:
-         for chunk in req.iter_content(chunk_size=50000):
-             #print(f'Received a Chunk')
-             fd.write(chunk)
+     req.raise_for_status()
+     with open('capthcas\captcha.jpg', 'wb') as fd:
+          for chunk in req.iter_content(chunk_size=50000):
+              #print(f'Received a Chunk')
+              fd.write(chunk)
              
             
     
-    x_test,names=load_data_one('capthcas')
-    output=model.predict(x_test)
-    x_test=[]
-    names=[]
-    #0-номер цифры в капче 1-номер входного значения  2-вероятность цифры(0-9)
-    answ=[]
-    for i in range(int(np.size(output[0])/np.size(output[0][0]))):
-        strq=''
-        for q in range(6):
-            #print(np.argmax(output[q][i]))
-            strq+=str(np.argmax(output[q][i]))
-        answ.append(strq)
-    login_details=answ[0]
+     x_test,names=load_data_one('capthcas')
+     output=model.predict(x_test)
+     x_test=[]
+     names=[]
+     #0-номер цифры в капче 1-номер входного значения  2-вероятность цифры(0-9)
+     answ=[]
+     for i in range(int(np.size(output[0])/np.size(output[0][0]))):
+         strq=''
+         for q in range(6):
+             #print(np.argmax(output[q][i]))
+             strq+=str(np.argmax(output[q][i]))
+         answ.append(strq)
+     login_details=answ[0]
     
     
-    params={'sid':2000,
-                       'form_name':'form',
-                       'DOC_SERIE':serie,
-                       'DOC_NUMBER':number,
-                       'captcha-input':login_details
-            }
-    r = s.post('http://services.fms.gov.ru/info-service.htm',params=params)           
-    soup = BeautifulSoup(r.content, features="html.parser")
-    #print(soup)
-    print(login_details)
-    #print('form_DOC_SERIE' not in r.text)
-    if ('form_DOC_SERIE' not in r.text)==True:
-        finish=True
+     params={'sid':2000,
+                        'form_name':'form',
+                        'DOC_SERIE':serie,
+                        'DOC_NUMBER':number,
+                        'captcha-input':login_details
+             }
+     r = s.post('http://services.fms.gov.ru/info-service.htm',params=params)           
+     soup = BeautifulSoup(r.content, features="html.parser")
+     #print(soup)
+     print(login_details)
+     #print('form_DOC_SERIE' not in r.text)
+     if ('form_DOC_SERIE' not in r.text)==True:
+         finish=True
+         
+         #continue
+ if counter==3:
+     return "Достигнуто максимальное количество попыток"
+ 
+ if TRIGGER_LINE in soup.text:
+     return True#print('С паспортом всё впорядке')
+ else:
+     return False#print('С паспортом проблемы')
         
-        #continue
-    
-if "Cреди недействительных не значится" in soup.text:
-    print('С паспортом всё впорядке')
-else:
-    print('С паспортом проблемы')
-        
-
+#print(check(11111,11111))
 
